8357f89f4dfe919ceb3241d6af28ca807a06ead9
Navneet-Indora <navneet.indora1@gmail.com>
2025-06-19 14:03:57 +0530

    Added first basic model

---
+++ src/main/java/org/example/uberreviewservice/models/Review.java
@@ -0,0 +1,13 @@
+package org.example.uberreviewservice.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Review {
+
+    @Id  //this annotation makes the id property a primary key of our table
+    Long id;
+
+}
